dc0e16ab927d1a792fa110457b216a77335a025e
Ben Alexander <1084972+mistahBen@users.noreply.github.com>
2022-06-09 10:59:17 -0500

    Update ipadreset.sh

--- ipadreset.sh
+++ ipadreset.sh
@@ -8,20 +8,15 @@ iPad reset script
 configpath=$HOME/Desktop/Configuration\ Profiles/studentwifinew.mobileconfig # edit this as needed
 
 while [ TRUE ] ; do
-read -p "press [ ENTER ] to reset iPads"
-devicecount = $(cfgutil list | wc -l)
-if [ "$devicecount" > 1 ] ; then
-    cfg="cfgutil -f"
-    else
-        cfg="cfgutil"
-fi
-$cfg erase
-sleep 5
-read -p "iPad(s) resetting. Press enter when the iPads are back at the welcome screen."
-$cfg install-profile "$configpath" && # Note: this command always report as failing even if it succeeds.
-sleep 30
-$cfg prepare --dep --skip-language --skip-region &&
+    read -p "press [ ENTER ] to reset iPads"
 
-afplay /System/Library/Sounds/Ping.aiff
-echo "You may disconnect iPads now. Press [ ENTER ] to configure next iPad(s). To exit, press Ctrl+C."
+    cfgutil -f erase &&
+    sleep 5
+    read -p "iPad(s) resetting. Press enter when the iPads are back at the welcome screen."
+    cfgutil -f install-profile "$configpath" && # Note: this command always report as failing even if it succeeds.
+    sleep 30
+    cfgutil -f prepare --dep --skip-language --skip-region &&
+
+    afplay /System/Library/Sounds/Ping.aiff
+    echo "You may disconnect iPads now. Press [ ENTER ] to configure next iPad(s). To exit, press Ctrl+C."
 done
